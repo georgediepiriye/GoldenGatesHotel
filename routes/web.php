014c99d6d57cc627f5a9428cb899dbde273f323e
George Diepiriye <georgediepiriye4u@gmail.com>
2021-08-21 01:33:06 +0100

    installed livewire and added admin dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Livewire\Admin\AdminDashboardComponent;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+//Admin
+Route::get('admin/dashboard',AdminDashboardComponent::class)->name('admin.dashboard');
